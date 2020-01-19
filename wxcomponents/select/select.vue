452c6354ad02e49ab6f8386ed0d5b2faa14535ef
RoyalYuan <303687320@qq.com>
2020-01-19 18:12:31 +0800

    导航框

--- wxcomponents/select/select.vue
+++ wxcomponents/select/select.vue
@@ -1,15 +1,17 @@
 <template>
-<!--pages/select.wxml-->
-<view class="com-selectBox">
-    <view class="com-sContent" @tap="selectToggle">
-        <view class="com-sTxt">{{nowText==" "?propArray[0].name:nowText}}
-        </view>
-        <image src="../../static/images/xia.png" class="com-sImg" :animation="animationData"></image>
-    </view>
-    <view class="com-sList" v-if="selectShow">
+	<view>
+<view class="index-header-bg">
+  <image src="/static/img/index-bg.png" mode="widthFix"></image>
+</view>
+<view class="index-header-box" @tap="selectToggle">
+  <image src="/static/img/area.png" class="area"></image>
+  <text>{{nowText==" "?propArray[0].name:nowText}}</text>
+  <image src="/static/img/down.png" class="down" :animation="animationData"></image>
+</view>
+   <view class="com-sList" v-if="selectShow">
         <view v-for="(item, index) in propArray" :key="index" :data-index="item" class="com-sItem" @tap="setText">{{item.name}}</view>
     </view>
-</view>
+	</view>
 </template>
 
 <script>
@@ -60,9 +62,6 @@ export default {
     },
     //设置内容
     setText: function (e) {
-      //console.log(e);
-      //var nowData = this.properties.propArray; //当前option的数据是引入组件的页面传过来的，所以这里获取数据只有通过this.properties
-
       var nowIdx = e.target.dataset.index; //当前点击的索引
       var nowText = e.target.dataset.index.name; //当前点击的内容
       //再次执行动画，注意这里一定，一定，一定是this.animation来使用动画
@@ -102,7 +101,6 @@ export default {
               that.$set(data, key2, {});
             }
           }
-
           data = data[key2];
         });
       });
